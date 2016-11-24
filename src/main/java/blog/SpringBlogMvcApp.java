b45b2bfce2cda296c5fa5863766cf62853becae0
Svetlosar Kovatchev <ivanovfuerte@yahoo.es>
2016-11-24 04:15:07 +0200

    Initial commit. Contains a working project. "Welcome to Spring MVC" should be displayed if you run the project.

---
+++ src/main/java/blog/SpringBlogMvcApp.java
@@ -0,0 +1,15 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ivanov on 24.11.2016 г..
+ */
+@SpringBootApplication
+public class SpringBlogMvcApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogMvcApp.class, args);
+    }
+
+}
---
+++ src/main/java/blog/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ivanov on 24.11.2016 г..
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
