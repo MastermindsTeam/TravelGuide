0f5a43c11531590460dbedb79ff0cd7b7fe3115f
PetrovStormwing <petrov_s_m@abv.bg>
2016-11-26 13:11:25 +0200

    Test

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String home(){
+
         return "home";
     }
 }
