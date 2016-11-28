dd238963ee847252d6b5bd7651c68969e1b27b2e
Svetlosar Kovatchev <ivanovfuerte@yahoo.es>
2016-11-28 06:18:55 +0200

    A new layout view added. New image BeFunkyCollage2.jpg added, yet needs to be scaled. Kristina's image program might be the best tool for this purpose. Java naming convention states short hyphen-linked names are preferred e.g. funky-collage.jpg and no white spaces are allowed.

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String home(){
-        return "home";
+        return "index";
     }
 }
