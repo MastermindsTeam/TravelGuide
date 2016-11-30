f7d9304324261c6ba0fb38ca2b1838487fed500c
Svetlosar Kovatchev <ivanovfuerte@yahoo.es>
2016-11-30 02:16:23 +0200

    Main view updated. It currently displays sample articles, along with their author(optional) and creation date. Ignore errors in index.html.

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -1,15 +1,31 @@
 package blog.controllers;
 
+import blog.models.Post;
+import blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by ivanov on 24.11.2016 г..
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model) {
+        List<Post> latest5Posts = postService.findLatest5();
+        model.addAttribute("latest5posts", latest5Posts);
+        List<Post> latest3Posts = latest5Posts.stream()
+                .limit(3).collect(Collectors.toList());
+        model.addAttribute("latest3posts", latest3Posts);
         return "index";
     }
 }
---
+++ src/main/java/blog/models/Post.java
@@ -0,0 +1,64 @@
+package blog.models;
+
+import java.util.Date;
+
+/**
+ * Created by ivanov on 30.11.2016 г..
+ */
+public class Post {
+    private Long id;
+    private String title;
+    private String body;
+    private User author;
+    private Date date = new Date();
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String body, User author) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
