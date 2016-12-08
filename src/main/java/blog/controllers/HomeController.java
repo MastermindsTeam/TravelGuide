775313fa495b03f4af7aa65301dc5ec591fceaa5
PetrovStormwing <petrov_s_m@abv.bg>
2016-12-08 18:37:20 +0200

    Modified HomeController.html       - added mapping to use the th:include {$view}       - changed some methods in order for the mapping to work  Created home/index.html  Created posts/details.html  Modified index.html        - removed all except tag main, in order to include it into base layout        - added th:each cycle for the articles display  Modified base-layout.html        - changed the tag main into a dynamic such  Modified layout.html        - all commented. Waiting for future editing

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -7,6 +7,7 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,27 +26,44 @@ public class HomeController {
     @Autowired
     private NotificationService notificationService;
 
-    @RequestMapping("/")
-    public String home(Model model) {
-
-        notificationService.addInfoMessage("Welcome to our amazing blog!");
-
-        List<Post> latest5Posts = postService.findLatest5();
-        model.addAttribute("latest5posts", latest5Posts);
-        List<Post> latest3Posts = latest5Posts.stream()
-                .limit(3).collect(Collectors.toList());
-        model.addAttribute("latest3posts", latest3Posts);
-        return "index";
-    }
+//    @RequestMapping("/")
+//    public String home(Model model) {
+//
+//        notificationService.addInfoMessage("Welcome to our amazing blog!");
+//
+//        List<Post> latest5Posts = postService.findLatest5();
+//        model.addAttribute("latest5posts", latest5Posts);
+//        List<Post> latest3Posts = latest5Posts.stream()
+//                .limit(3).collect(Collectors.toList());
+//        model.addAttribute("latest3posts", latest3Posts);
+//
+//        return "base-layout";
+//    }
 
     @RequestMapping("/posts/view/{id}")
-    public String view(@PathVariable("id") Long id, Model model){
+    public String view(@PathVariable("id") Long id, Model model) {
         Post post = postService.findById(id);
-        if(post == null){
+        if (post == null) {
             notificationService.addErrorMessage("Cannot find post: " + id);
             return "redirect:/";
         }
+
         model.addAttribute("post", post);
-        return "/posts/index";
+        model.addAttribute("view", "posts/index");
+
+        return "base-layout";
+    }
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        notificationService.addInfoMessage("Welcome to our amazing blog!");
+
+        List<Post> latest5Posts = this.postService.findLatest5();
+
+        model.addAttribute("latest5posts", latest5Posts);
+        model.addAttribute("view", "home/index");
+
+        return "base-layout";
     }
 }
