06eb93472b340c1277d9625fdca688813d3eddce
PetrovStormwing <petrov_s_m@abv.bg>
2016-12-12 16:22:02 +0200

    Cleared unused files

--- src/main/java/blog/services/LoginService.java
+++
@@ -1,8 +0,0 @@
-package blog.services;
-
-/**
- * Created by ivanov on 6.12.2016 г..
- */
-public interface LoginService {
-    boolean authenticate(String username, String password);
-}
--- src/main/java/blog/services/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package blog.services;
-
-/**
- * Created by ivanov on 4.12.2016 г..
- */
-public interface NotificationService {
-    void addInfoMessage(String msg);
-    void addErrorMessage(String msg);
-}
