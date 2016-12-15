528b5f4d2e73e32debc1f61a2e72f9eead1bd27f
Svetlosar Kovatchev <ivanovfuerte@yahoo.es>
2016-12-15 04:52:12 +0200

    Categories are displayed on the home page instead of articles.

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -2,37 +2,56 @@ package blog.controllers;
 
 
 import blog.entity.Article;
+import blog.entity.Category;
 import blog.repository.ArticleRepository;
+import blog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Set;
 
 /**
- * Created by PetrovStormwing on 12/Dec/2016.
+ * Created by ivanov on 15/Dec/2016.
  */
 @Controller
 public class HomeController {
 
     @Autowired
-    private ArticleRepository articleRepository;
+    private CategoryRepository categoryRepository;
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Article> articles=this.articleRepository.findAll();
+        List<Category> categories = this.categoryRepository.findAll();
 
-        model.addAttribute("articles", articles);
+        model.addAttribute("categories", categories);
         model.addAttribute("view", "home/index");
 
         return "base-layout";
     }
 
     @RequestMapping("/error/403")
-    public String accessDenied(Model model){
+    public String accessDenied(Model model) {
         model.addAttribute("view", "error/403");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/category/{id}")
+    public String listArticles(Model model, @PathVariable Integer id){
+        if(!this.categoryRepository.exists(id)){
+            return "redirect:/";
+        }
+        Category category = this.categoryRepository.findOne(id);
+        Set<Article> articles = category.getArticles();
+
+        model.addAttribute("articles", articles);
+        model.addAttribute("category", category);
+        model.addAttribute("view", "home/list-articles");
 
         return "base-layout";
     }
